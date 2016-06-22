b037c032fe387da47501040b72e1201d1f43946a
ERON <2478327261@qq.com>
2016-06-22 19:21:33 +0800

    ERON

--- src/RadarNavigation/RadarNavigation.java
+++ src/RadarNavigation/RadarNavigation.java
@@ -1,9 +1,7 @@
 package RadarNavigation;
 
-import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
