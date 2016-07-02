b1b617e620e4bb2d92bcde42e291476c90c2f5c1
ERON <2478327261@qq.com>
2016-07-02 21:09:54 +0800

    ERON

--- src/shipserver/SmallPanel.java
+++ src/shipserver/SmallPanel.java
@@ -1,6 +1,7 @@
 package shipserver;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -10,14 +11,28 @@ import common.Ship;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import javax.swing.JOptionPane;
+import java.awt.Font;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseWheelListener;
+import java.awt.event.MouseWheelEvent;
 
 @SuppressWarnings("serial")
-public class SmallPanel extends JPanel { // 这个类不需要了， 采用原来的设计
+public class SmallPanel extends JPanel implements Runnable{ // 这个类不需要了， 采用原来的设计
+    
+    private double mousex, mousey;  //移动鼠标的坐标
+    private double dragx, dragy;    //拖动未松开时鼠标坐标
+    private double oldx, oldy;    //按下鼠标后   坐标
+    private double newx, newy;    //松开鼠标   坐标
+    private double delx, dely;   //删除时鼠标的坐标
+    private String type = "Normal";            //船舶类型以后可以添加
+    //在界面上现实的额帮助说明
+    String helpStr = "";
+    String nameStr = "", positionStr = "", speedStr = "", courseStr = "", typeShow = "";
 	
 	private List<Socket> sockets = new LinkedList<Socket>(); // 连接的套接字对象
 	private List<Ship> clientShips = new LinkedList<Ship>(); // 所有客户端和服务端产生的船舶维护对象
@@ -27,9 +42,86 @@ public class SmallPanel extends JPanel { // 这个类不需要了， 采用原来的设计
 	
 	public SmallPanel() {
 		super();
+		addMouseWheelListener(new MouseWheelListener() {
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				setCursor(new Cursor(Cursor.MOVE_CURSOR));  //移动光标
+				
+			}
+		});
+		addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+				mousex = e.getX();
+				mousey = e.getY();
+				helpStr = "'Left Click & Drag' Create Ships, 'Right Click' Delete, 'C' Button Change Type";
+				repaint();
+			}
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				dragx = e.getX();
+				dragy = e.getY();
+				//用paint进行重新绘制
+				helpStr = "Drag to Create Moving Ship, 'Right Double Click' Delete All Ship";
+				repaint();
+			}
+		});
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getButton() == MouseEvent.BUTTON1) {
+					oldx = e.getX();
+					oldy = e.getY();
+				}
+				if (e.getButton() == MouseEvent.BUTTON3) {
+					delx = e.getX();
+					dely = e.getY();
+				}
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+			}
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				if(e.getButton() == MouseEvent.BUTTON1){
+		            newx = e.getX();
+		            newy = e.getY();
+		            double course = CaculateRatio(mousex, mousey, newx, newy);
+		            double differentx = newx - mousex;
+		            double differenty = newy - mousey;
+		            double speed = Math.sqrt(Math.pow(differentx, 2) + Math.pow(differenty, 2))/10;
+		            //弹出参数填充窗口
+		            String name = JOptionPane.showInputDialog("请输入船名 ： ");
+		            if (name == null) {   //如果返回空值，则不进行动作
+						return;
+					}
+		            Ship ship = new Ship(name, mousex, mousey, speed, course, type);
+		            serverShips.add(ship);
+		            switch(ship.getType()){
+		                /*case 0: typeShow = "Normal";break;
+		                case 1: typeShow = "Sailing";break;
+		                case 2: typeShow = "Fishing";break;
+		                case 3: typeShow = "Out of Control";break;
+		                case 4: typeShow = "Limit by Control";break;
+		                case 5: typeShow = "Limit by Draft";break;*/
+		            }
+		            //在下方显示的帮助文字
+		            helpStr = "A Ship Exist";
+		            //右上方，显示的当前船舶信息
+		            nameStr = "Ship name : "+name;
+		            positionStr = "Position : "+mousex+","+mousey;
+		            speedStr = "Speed : "+(int)speed;
+		            courseStr = "Course : "+(int)course;
+		            typeShow = "Type : "+typeShow;
+		        }
+				new Thread(SmallPanel.this).start();
+			}
+		});
+		
 		initComponents();
-		clientShips.add(new Ship("shejghj", 123, 345, 23, 15, "limit"));
-		clientShips.add(new Ship("shejghj", 500, 234, 12, 15, "limit"));
+		//测试
+		clientShips.add(new Ship("shejghj", 123, 345, 12, 7, "limit"));
+		clientShips.add(new Ship("shejghj", 500, 234, 27, 9, "limit"));
 	}
 	private void initComponents() {
 		// TODO Auto-generated constructor stub
@@ -37,9 +129,42 @@ public class SmallPanel extends JPanel { // 这个类不需要了， 采用原来的设计
 		//setOpaque(false); // 设置成透明的 opaque不透明
 		setBackground(Color.WHITE);
 	}
-
-	/************** 通用程序区 *********************************************/
 	
+	/************** 通用程序区 *********************************************/
+	/**
+	 * 通过两点-->计算出两点斜率,方向--》向上为0， 顺时针旋转    到  360°
+	 * @param start_x  起点 x
+	 * @param start_y  起点  y
+	 * @param end_x  终点  x
+	 * @param end_y  终点  y
+	 * @return  两点之间的斜率
+	 */
+	private double CaculateRatio(double start_x, double start_y, double end_x, double end_y){
+        double differentx = end_x - start_x;
+        double differenty = end_y - start_y;
+        double course = 0;
+        int adjust = 0;//switch case///
+        
+        if(differentx == 0 && differenty == 0) adjust = 0;
+        else if(differentx >= 0 && differenty <0) adjust = 1;
+        else if(differentx < 0 && differenty <= 0) adjust = 2;
+        else if(differentx <= 0 && differenty > 0) adjust = 3;
+        else if(differentx > 0 && differenty >= 0) adjust = 4;
+        
+        switch(adjust){
+            case 0 : course = 0; break;
+            case 1 : course = 450 - Math.toDegrees(Math.atan2(-differenty, differentx)); break;
+            case 2 : course = 90 - Math.toDegrees(Math.atan2(-differenty, differentx)); break;
+            case 3 : course = 90 - Math.toDegrees(Math.atan2(-differenty, differentx)); break;
+            case 4 : course = 90 - Math.toDegrees(Math.atan2(-differenty, differentx)); break;
+        }
+        
+        while (course<0||course>=360){
+            if(course<0) course+=360;
+            if(course>=360) course-=360;
+        }
+        return course;
+    }
 	
 	/********************* 图形绘制区 **********************************/
 	@Override
@@ -47,9 +172,11 @@ public class SmallPanel extends JPanel { // 这个类不需要了， 采用原来的设计
 		// TODO Auto-generated method stub
 		super.paint(g);
 		Graphics2D g2 = (Graphics2D) g;
+		g2.setFont(new Font("Default", Font.PLAIN, (int) (Math.min(getWidth(), getHeight())*0.03)));
 		g2.setColor(Color.BLUE);
 		
 		paintShips(g2);
+		printString(g2);
 	}
 	
 	public void paintShips(Graphics2D g2) {
@@ -74,7 +201,7 @@ public class SmallPanel extends JPanel { // 这个类不需要了， 采用原来的设计
 			course = Math.toRadians(vessel.getParameter(3));
 			speed = vessel.getParameter(4);
 		}
-
+		
 	}
 	
 	public void normalShip(Graphics2D g2, double Px, double Py, double course, double speed) {
@@ -100,12 +227,29 @@ public class SmallPanel extends JPanel { // 这个类不需要了， 采用原来的设计
 	public void printString(Graphics g2){
         g2.setColor(Color.BLACK);
         //auto hide
-        g2.drawString(mousex + " , " + mousey, 20, 680);//mouse position 820,  680
-        g2.drawString(helpStr, 170, 680);//help position
+        g2.drawString(mousex + " , " + mousey, 20, getHeight()-30);//mouse position 820,  680
+        g2.drawString(helpStr, 170, getHeight()-30);//help position
         
-        g2.drawString(positionStr, 850, 25);//infomation showing
-        g2.drawString(speedStr, 850, 50);
-        g2.drawString(courseStr, 850, 75);
+        g2.drawString(nameStr, 850, 25);
+        g2.drawString(positionStr, 850, 50);
+        g2.drawString(speedStr, 850, 75);
+        g2.drawString(courseStr, 850, 100);
     }
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			Thread.sleep(1000);      //调整显示时间
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
+		nameStr = "";
+		positionStr = "";
+		speedStr = "";
+		courseStr = "";
+		repaint();
+	}
 	
 }
