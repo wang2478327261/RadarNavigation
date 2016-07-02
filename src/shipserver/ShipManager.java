32098ee0480c0d4ffe7002e8f41897c04eaff64c
ERON <2478327261@qq.com>
2016-07-02 12:15:19 +0800

    ERON

--- src/shipserver/ShipManager.java
+++ src/shipserver/ShipManager.java
@@ -31,13 +31,7 @@ import java.awt.event.ComponentEvent;
 @SuppressWarnings("serial")
 public class ShipManager extends JFrame{   //服务端需要添加船舶的功能，方便测试
 	
-	private JPanel contentPane;
-	private List<Socket> sockets = new LinkedList<Socket>();  //连接的套接字对象
-	private List<Ship> clientShips = new LinkedList<Ship>();   //所有客户端和服务端产生的船舶维护对象
-	private List<Ship> serverShips = new LinkedList<Ship>();   //服务端生成的对象
-	
-	//功能需要     变量区域
-	private boolean fullScreen = false;
+	private SmallPanel smallpanel;
 	
 	/**
 	 * Launch the application.
@@ -70,71 +64,11 @@ public class ShipManager extends JFrame{   //服务端需要添加船舶的功能，方便测试
 	private void initComponents() {
 		setTitle("ShipManager");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 500);
-		contentPane = new JPanel();
-		contentPane.setBorder(BorderFactory.createEmptyBorder());
-		contentPane.setLayout(null);
-		setContentPane(contentPane);
-		
-		clientShips.add(new Ship("dheu",123,23,23,14,"fr"));
-	}
-
-	@Override
-	public void print(Graphics g) {
-		// TODO Auto-generated method stub
-		super.print(g);
-		Graphics2D g2 = (Graphics2D)g.create();
-		
-		paintShips(g2);
+		setBounds(20, 20, 1008, 735);
+		smallpanel = new SmallPanel();
+		smallpanel.setBorder(BorderFactory.createEmptyBorder());
+		smallpanel.setLayout(null);
+		setContentPane(smallpanel);
 	}
 	
-	public void paintShips(Graphics2D g2){
-		
-		g2.setColor(Color.BLACK);
-        double Px, Py, course, speed;
-        //
-        for(Ship vessel : clientShips){
-            Px = vessel.getParameter(1);
-            Py = vessel.getParameter(2);
-            course = Math.toRadians(vessel.getParameter(3));
-            speed = vessel.getParameter(4);
-            
-            switch(vessel.getType()){
-            //根据船舶类型的不同进行绘制
-                /*case 0: normalShip(x, y, speed, c,g); break;
-                case 1: sailingShip(x, y, speed, c,g); break;
-                case 2: fishingShip(x, y, speed, c,g); break;
-                case 3: outofControl(x, y, speed, c,g); break;
-                case 4: limitbyControl(x, y, speed, c,g); break;
-                case 5: limitbuDraft(x, y, speed, c,g); break;*/
-            }
-            normalShip(g2, Px, Py, course, speed);
-            
-        }
-        
-    }
-	
-	public void normalShip(Graphics2D g2, double Px, double Py, double course, double speed){
-        int linestartx, linestarty, lineendx, lineendy;
-        linestartx = (int)(Px + 20*Math.sin(course));
-        linestarty = (int)(Py - 20*Math.cos(course));
-        lineendx = (int) (linestartx + speed*Math.sin(course));
-        lineendy = (int) (linestarty - speed*Math.cos(course));
-        
-        int[] trianglex = {linestartx, 
-            (int)(Px + 7*Math.sin(course+Math.PI/2)),
-            (int)(Px-10*Math.sin(course)+7*Math.sin(course+Math.PI/2)),
-            (int)(Px-10*Math.sin(course)+7*Math.sin(course+3*Math.PI/2)),
-            (int)(Px + 7*Math.sin(course+3*Math.PI/2))
-        };
-        int[] triangley = {linestarty, 
-            (int)(Py - 7*Math.cos(course+Math.PI/2)), 
-            (int)(Py+10*Math.cos(course)-7*Math.cos(course+Math.PI/2)),
-            (int)(Py+10*Math.cos(course)-7*Math.cos(course+3*Math.PI/2)),
-            (int)(Py - 7*Math.cos(course+3*Math.PI/2))
-        };
-        //drawbody and courseline
-        g2.drawPolygon(trianglex, triangley, 5);
-        g2.drawLine(linestartx, linestarty, lineendx, lineendy);
-    }
 }
--- src/shipserver/SmallPanel.java
+++ src/shipserver/SmallPanel.java
@@ -3,59 +3,109 @@ package shipserver;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import common.Ship;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings("serial")
-public class SmallPanel extends JPanel{    //这个类不需要了， 采用原来的设计
+public class SmallPanel extends JPanel { // 这个类不需要了， 采用原来的设计
 	
-	List<Ship> ships = new LinkedList<Ship>();
+	private List<Socket> sockets = new LinkedList<Socket>(); // 连接的套接字对象
+	private List<Ship> clientShips = new LinkedList<Ship>(); // 所有客户端和服务端产生的船舶维护对象
+	private List<Ship> serverShips = new LinkedList<Ship>(); // 服务端生成的对象
+	//存储船舶轨迹
+	private Map<Ship, List<Point>> track = new HashMap<Ship, List<Point>>();   //一条船对应一条轨迹链
 	
 	public SmallPanel() {
 		super();
 		initComponents();
+		clientShips.add(new Ship("shejghj", 123, 345, 23, 15, "limit"));
+		clientShips.add(new Ship("shejghj", 500, 234, 12, 15, "limit"));
 	}
 	private void initComponents() {
-		addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				setBorder(BorderFactory.createLineBorder(Color.BLUE, 1));
-				//setOpaque(true);  //设置成不透明
-			}
-			@Override
-			public void mouseExited(MouseEvent e) {
-				setBorder(BorderFactory.createEmptyBorder());
-				//setOpaque(false);   //设置成透明
-			}
-		});
 		// TODO Auto-generated constructor stub
-		
 		setBorder(BorderFactory.createEmptyBorder());
-		setOpaque(false);  //设置成透明的       opaque不透明
+		//setOpaque(false); // 设置成透明的 opaque不透明
 		setBackground(Color.WHITE);
 	}
+
+	/************** 通用程序区 *********************************************/
 	
-	/**************通用程序区*********************************************/
-	public void addShip(Ship ship) {  //是通过船舶名称还是对象传入？
-		ships.add(ship);
-	}
-	public void removeShip(Ship ship){
-		ships.remove(ship);
-	}
 	
-	/*********************图形画画区**********************************/
+	/********************* 图形绘制区 **********************************/
 	@Override
 	public void paint(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paint(g);
-		Graphics2D g2 = (Graphics2D)g;
+		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(Color.BLUE);
-		g2.drawString("hello worls", 0, 20);
+		
+		paintShips(g2);
 	}
+	
+	public void paintShips(Graphics2D g2) {
+		
+		double Px, Py, course, speed;
+		g2.setColor(Color.BLACK);
+		for (Ship vessel : clientShips) {
+			Px = vessel.getParameter(1);
+			Py = vessel.getParameter(2);
+			course = Math.toRadians(vessel.getParameter(3));
+			speed = vessel.getParameter(4);
+			
+			switch (vessel.getType()) {
+			// 根据船舶类型的不同进行绘制
+			}
+			normalShip(g2, Px, Py, course, speed);
+			
+		}
+		for (Ship vessel : serverShips) {
+			Px = vessel.getParameter(1);
+			Py = vessel.getParameter(2);
+			course = Math.toRadians(vessel.getParameter(3));
+			speed = vessel.getParameter(4);
+		}
+
+	}
+	
+	public void normalShip(Graphics2D g2, double Px, double Py, double course, double speed) {
+		int linestartx, linestarty, lineendx, lineendy;
+		linestartx = (int) (Px + 20 * Math.sin(course));
+		linestarty = (int) (Py - 20 * Math.cos(course));
+		lineendx = (int) (linestartx + speed * Math.sin(course));
+		lineendy = (int) (linestarty - speed * Math.cos(course));
+		
+		int[] trianglex = { linestartx, (int) (Px + 7 * Math.sin(course + Math.PI / 2)),
+				(int) (Px - 10 * Math.sin(course) + 7 * Math.sin(course + Math.PI / 2)),
+				(int) (Px - 10 * Math.sin(course) + 7 * Math.sin(course + 3 * Math.PI / 2)),
+				(int) (Px + 7 * Math.sin(course + 3 * Math.PI / 2)) };
+		int[] triangley = { linestarty, (int) (Py - 7 * Math.cos(course + Math.PI / 2)),
+				(int) (Py + 10 * Math.cos(course) - 7 * Math.cos(course + Math.PI / 2)),
+				(int) (Py + 10 * Math.cos(course) - 7 * Math.cos(course + 3 * Math.PI / 2)),
+				(int) (Py - 7 * Math.cos(course + 3 * Math.PI / 2)) };
+		// drawbody and courseline
+		g2.drawPolygon(trianglex, triangley, 5);
+		g2.drawLine(linestartx, linestarty, lineendx, lineendy);
+	}
+	
+	public void printString(Graphics g2){
+        g2.setColor(Color.BLACK);
+        //auto hide
+        g2.drawString(mousex + " , " + mousey, 20, 680);//mouse position 820,  680
+        g2.drawString(helpStr, 170, 680);//help position
+        
+        g2.drawString(positionStr, 850, 25);//infomation showing
+        g2.drawString(speedStr, 850, 50);
+        g2.drawString(courseStr, 850, 75);
+    }
 	
 }
