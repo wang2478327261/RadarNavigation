8e4f6601946fd44c927c3c1118458ea3e5e90ee4
ERON <2478327261@qq.com>
2017-03-06 22:11:03 +0800

    ok, not to thinking a lot

--- src/shipserver/ShipManager.java
+++ src/shipserver/ShipManager.java
@@ -4,43 +4,41 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
-public class ShipManager extends JFrame{
-	
-	private static final long serialVersionUID = 5649146024506368826L;
-	
-	private SmallPanel smallpanel;
-	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ShipManager frame = new ShipManager();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	
-	/**
-	 * Create the frame.
-	 */
-	public ShipManager() {
-		initComponents();
-	}
-	
-	private void initComponents() {
-		setTitle("ShipManager");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(20, 20, 1208, 735);
-		smallpanel = new SmallPanel();
-		//smallpanel.setBorder(BorderFactory.createEmptyBorder());
-		//smallpanel.setLayout(null);
-		setContentPane(smallpanel);
-	}
-	
+public class ShipManager extends JFrame {
+
+    private static final long serialVersionUID = 5649146024506368826L;
+
+    private SmallPanel smallpanel;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    ShipManager frame = new ShipManager();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    System.exit(1);
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public ShipManager() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        setTitle("ShipManager");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(20, 20, 1208, 735);
+        smallpanel = new SmallPanel();
+        setContentPane(smallpanel);
+    }
+
 }
